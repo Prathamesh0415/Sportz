3c512e75df1d76cca493cc4f1212b5da6ef27709
Prathamesh Ghormare <prathameshghormare@gmail.com>
2026-02-05 21:20:52 +0530

    add routes to get matches and post matches

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -1,7 +1,64 @@
 import { Router } from 'express'
+import { db } from '../db/db.js';
+import { createMatchSchema } from '../validation/matches.js';
+import { matches } from '../db/schema.js'
+import { getMatchStatus } from '../utils/matchStatus.js';
+import { listMatchesQuerySchema } from '../validation/matches.js';
+import {desc} from "drizzle-orm";
 
 export const matchRouter = Router()
 
-matchRouter.get('/', (req, res) => {
-    res.status(200).json({message: "matches list"})
+const MAX_LIMIT = 100
+
+matchRouter.get('/', async (req, res) => {
+    const parsed = listMatchesQuerySchema.safeParse(req.query);
+
+    if (!parsed.success) {
+        return res.status(400).json({error: 'Invalid query.', details: parsed.error.issues });
+    }
+
+    const limit = Math.min(parsed.data.limit ?? 50, MAX_LIMIT);
+
+    try {
+        const data = await db
+            .select()
+            .from(matches)
+            .orderBy((desc(matches.createdAt)))
+            .limit(limit)
+
+        res.json({ data });
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({ error: 'Failed to list matches.' });
+    }
+})
+
+matchRouter.post('/', async (req, res) => {
+    const parsed = createMatchSchema.safeParse(req.body);
+
+    if(!parsed.success) {
+        return res.status(400).json({ error: 'Invalid payload.', details: parsed.error.issues });
+    }
+
+    const { data: { startTime, endTime, homeScore, awayScore } } = parsed;
+
+    try {
+        const [event] = await db.insert(matches).values({
+            ...parsed.data,
+            startTime: new Date(startTime),
+            endTime: new Date(endTime),
+            homeScore: homeScore ?? 0,
+            awayScore: awayScore ?? 0,
+            status: getMatchStatus(startTime, endTime),
+        }).returning();
+
+        if(res.app.locals.broadcastMatchCreated) {
+            res.app.locals.broadcastMatchCreated(event);
+        }
+
+        res.status(201).json({ data: event });
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({ error: 'Failed to create match.', details: JSON.stringify(e) });
+    }
 })
